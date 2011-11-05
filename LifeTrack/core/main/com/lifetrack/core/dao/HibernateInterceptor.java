aaad67f497059420c678f619ce6ea9f989fb2ad2
charles.fy0708@gmail.com <charles.fy0708@gmail.com@f32955b9-79fb-9ab9-a7f4-2677f1794030>
2011-11-05 11:52:57 +0000

    一些修改

--- LifeTrack/core/main/com/lifetrack/core/dao/HibernateInterceptor.java
+++ LifeTrack/core/main/com/lifetrack/core/dao/HibernateInterceptor.java
@@ -9,6 +9,12 @@ import org.hibernate.type.Type;
 import com.lifetrack.core.exception.IllegalEntityExcpetion;
 import com.lifetrack.core.validator.BeanValidator;
 
+/**
+ * hibernate拦截器，这里只是在向数据库插入数据时填充创建时间字段
+ * 
+ * @author Charles Feng
+ * @since 1.0.0
+ */
 public class HibernateInterceptor extends EmptyInterceptor {
 
 	private static final long serialVersionUID = 4426550787363797218L;
