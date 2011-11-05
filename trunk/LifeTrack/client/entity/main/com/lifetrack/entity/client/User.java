86c0925c3d7244fe632c8f849883348513681fbe
charles.fy0708@gmail.com <charles.fy0708@gmail.com@f32955b9-79fb-9ab9-a7f4-2677f1794030>
2011-11-05 16:27:53 +0000

    User Info

--- trunk/LifeTrack/client/entity/main/com/lifetrack/entity/client/User.java
+++ trunk/LifeTrack/client/entity/main/com/lifetrack/entity/client/User.java
@@ -1,5 +1,7 @@
 package com.lifetrack.entity.client;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,5 +12,18 @@ import com.lifetrack.core.entity.BaseEntity;
 @Entity
 @Table(name = "user")
 public class User extends BaseEntity {
+	
+	private String name;
+	private int loginTimes;
+	private Date lastLoginTime;
+	
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
